fd648b8d1d092bb0311e1c33e09e8e20be8d9520
Karen Stewart <kastewart2002@googlemail.com>
2021-09-10 15:20:44 +0100

    first test passed

--- src/pet_shop.py
+++ src/pet_shop.py
@@ -1,1 +1,9 @@
 # WRITE YOUR FUNCTIONS HERE
+
+
+def get_pet_shop_name(pet_list):
+    shop_name = pet_list["name"]
+    return shop_name
+
+
+
--- tests/pet_shop_test.py
+++ tests/pet_shop_test.py
@@ -1,4 +1,4 @@
-+import unittest
+import unittest
 from src.pet_shop import *
 
 class TestPetShop(unittest.TestCase):
