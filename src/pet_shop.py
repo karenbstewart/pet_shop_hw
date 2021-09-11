7d31ba696bc30fab015e9205832ddac47b489cb7
Karen Stewart <kastewart2002@googlemail.com>
2021-09-11 16:22:29 +0100

    test 18 passed - optional test 1

--- src/pet_shop.py
+++ src/pet_shop.py
@@ -79,6 +79,14 @@ def add_pet_to_customer(customer, new_pet):
 
 #----- OPTIONAL TESTS -------
 
+#test 18 
+
+def customer_can_afford_pet(customer, new_pet):
+    if customer["cash"] >= new_pet["price"]:
+        return True
+    
+    return False
+
 
 
 
