5f4210cb905c60c61ed76e945e4d512513c62883
Karen Stewart <kastewart2002@googlemail.com>
2021-09-11 14:10:33 +0100

    second test passed

--- src/pet_shop.py
+++ src/pet_shop.py
@@ -1,9 +1,12 @@
 # WRITE YOUR FUNCTIONS HERE
-
+import pdb
 
 def get_pet_shop_name(pet_list):
     shop_name = pet_list["name"]
     return shop_name
 
+def get_total_cash(pet_list):
+    #pdb.set_trace()
+    return pet_list["admin"]["total_cash"]
 
 
